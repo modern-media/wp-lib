72731994a3ce1317fabd711e6864fe6d01320375
Christopher Carson <chris@modernmedia.co>
2014-01-30 09:06:58 -0600

    working on theme

---
+++ src/ModernMedia/WPLib/SocialSharing/Data/GooglePlusShareParams.php
@@ -0,0 +1,12 @@
+<?php
+namespace ModernMedia\WPLib\SocialSharing\Data;
+
+use ModernMedia\WPLib\Data\BaseData;
+
+class GooglePlusShareParams extends BaseData {
+
+	public $href = '';
+
+	public $annotation = 'none';
+
+} 
