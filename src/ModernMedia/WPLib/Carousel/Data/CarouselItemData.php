fdffed335e7b87ea415b48d6be448637d9f82120
Christopher Carson <chris@modernmedia.co>
2014-02-03 12:13:47 -0600

    updated composer.json

--- src/ModernMedia/WPLib/Carousel/Data/CarouselItemData.php
+++ src/ModernMedia/WPLib/Carousel/Data/CarouselItemData.php
@@ -2,6 +2,9 @@
 namespace ModernMedia\WPLib\Carousel\Data;
 use ModernMedia\WPLib\Data\BaseData;
 class CarouselItemData extends BaseData {
-
+	public $image_id;
+	public $link_image_to;
+	public $header_text;
+	public $text;
 
 } 
